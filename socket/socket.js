4feb592ad150f4ba61586578a1968d6855f6ce2f
Lemursaurio <ggarro_1303@hotmail.com>
2024-12-11 18:48:45 -0500

    feat: socket io funcionando

--- socket/socket.js
+++ socket/socket.js
@@ -1,4 +1,5 @@
 const socketIo = require('socket.io');
+const Message = require('../models/message')
 
 module.exports = (server) => {
     const io = socketIo(server);
@@ -6,8 +7,16 @@ module.exports = (server) => {
     io.on('connection', (socket) => {
         console.log('Un cliente se ha conectado:', socket.id)
 
-        socket.on('message', (data) => {
+        socket.on('message', async (data) => {
             console.log('Mensaje recibido:', data)
+            const dataMessage = {
+                communityId: "674b5a25285b053e3cb60e39",
+                roomId: "6758bb6044ab02804ce88bbf",
+                user: "Gabriel Garro",
+                message: data.message
+            }
+            const newMessage = new Message(dataMessage)
+            await newMessage.save()
             io.emit('message', data)
         })
     })
